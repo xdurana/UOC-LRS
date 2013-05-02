764d5082f44f9cd213cff487d166950da47b0f51
Xavier Duran <xdurana@uoc.edu>
2013-05-02 12:45:14 +0200

    Experience API Skeleton

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
+var express = require('express');
 
-/**
- * Module dependencies.
- */
+var routes = require('./routes');
+var experiences = require('./routes/experience');
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , experience = require('./routes/experience')
-  , http = require('http')
-  , path = require('path');
+var http = require('http')
+var path = require('path');
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -23,14 +18,14 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/experiences', experience.list);
+app.get('/experiences', experiences.all);
+app.get('/experiences/:name', experiences.one);
+app.post('/experiences', experiences.insert);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/experience.js
+++ routes/experience.js
@@ -1,8 +1,22 @@
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/test');
+var experiences = db.get('experiences');
 
-/*
- * GET experiences listing.
- */
+exports.all = function(req, res) {
+	experiences.find({}, {limit:20}, function (e, docs) {
+		res.json(docs)
+	})
+};
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+exports.one = function(req, res) {
+  experiences.find({"actor.name": req.params.name}, {limit: 20}, function(e,docs) {
+   	res.json(docs);
+ 	})
+};
+
+exports.insert = function(req, res) {
+	experiences.insert(req.body, function (e, doc) {
+	  res.send(req.body);
+	});
 };
